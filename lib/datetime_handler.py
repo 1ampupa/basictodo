d191d25241ba5f530a381d307ca4982bdf167c03
1ampupa <craftpupathai@gmail.com>
2025-10-21 00:30:11 +0700

    feat(datetime-handler): Add Datetime Query, Calculation, Comparation functions

--- lib/datetime_handler.py
+++ lib/datetime_handler.py
@@ -5,7 +5,7 @@ class DateTime:
 
     # Format Config
     _DateFormat = "%d/%m/%Y"
-    _FullDateFormat = "$A %d/%m/%Y"
+    _FullDateFormat = "%A %d/%m/%Y"
     _TimeFormat = "%H:%M"
     _FullTimeFormat = "%H:%M:%S"
 
@@ -30,21 +30,46 @@ class DateTime:
             return self.now.strftime(DateTime._FullTimeFormat)
         return self.now.strftime(DateTime._TimeFormat)
     
+    def GetTodayAsString(self, IncludeWeekday) -> str:
+        if IncludeWeekday:
+            return self.now.strftime(DateTime._FullDateFormat)
+        return self.now.strftime(DateTime._DateFormat)
+    
+    def GetDateTimeAsDict(self) -> dict:
+        return {
+            "date": self.fullDate.isoformat(),
+            "time": self.fullTime.strftime(self._TimeFormat)
+        }
+    
     # Date Time Calculation Functions
 
-    def Add(self, attribute : str, value : int) -> datetime:
+    def Add(self, _Attribute : str, _Value : int) -> datetime:
         try:
-            _New_Time = self.now + timedelta(**{attribute:value})
+            _New_Time = self.now + timedelta(**{_Attribute:_Value})
             return _New_Time
         except Exception as _E:
             print(_E)
 
-    def Subtract(self, attribute : str, value : int) -> datetime:
+    def Subtract(self, _Attribute : str, _Value : int) -> datetime:
         try:
-            _New_Time = self.now - timedelta(**{attribute:value})
+            _New_Time = self.now - timedelta(**{_Attribute:_Value})
             return _New_Time
         except Exception as _E:
             print(_E)
+
+    # Date Time Comparation Functions
+    def DiffWithDateTime(self, _OtherDatetime : datetime, _Unit : str) -> int:
+        _Diff = self.now - _OtherDatetime
+        _Diff_In_Seconds = _Diff.total_seconds()
+
+        match _Unit.lower():
+            case "seconds": return int(_Diff_In_Seconds)
+            case "minutes": return int(_Diff_In_Seconds // 60)
+            case "hours": return int(_Diff_In_Seconds // 3600)
+            case "days": return _Diff.days
+            case _:
+                print("Error in function 'DateTime.DiffWithDateTime': Invalid unit received. Return day difference.")
+                return _Diff.days
 
     def __str__(self):
         return f"{self.fullTime} {self.fullDate}"
