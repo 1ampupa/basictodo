3467c084a879751eda88302144e17d9c15cec6d4
1ampupa <craftpupathai@gmail.com>
2025-10-21 18:35:34 +0700

    refactor(datetime-handler): Refactor Add Subtract Diff... functions.

--- lib/datetime_handler.py
+++ lib/datetime_handler.py
@@ -4,14 +4,15 @@ class DateTime:
     # Class Variables
 
     # Format Config
-    _DateFormat = "%d/%m/%Y"
-    _FullDateFormat = "%A %d/%m/%Y"
-    _TimeFormat = "%H:%M"
-    _FullTimeFormat = "%H:%M:%S"
+    _DateFormat : str = "%d/%m/%Y"
+    _FullDateFormat : str = "%A %d/%m/%Y"
+    _TimeFormat : str = "%H:%M"
+    _FullTimeFormat : str = "%H:%M:%S"
 
     # Constructor
-    def __init__(self):
-        self.now : datetime = datetime.now()
+    def __init__(self, DateTime: datetime = None):
+        self.now : datetime = DateTime or datetime.now()
+
         self.year : int = int(self.now.strftime("%Y"))
         self.month : int = int(self.now.strftime("%m"))
         self.date : int = int(self.now.strftime("%d"))
@@ -20,69 +21,81 @@ class DateTime:
         self.minute : int = int(self.now.strftime("%M"))
         self.second : int = int(self.now.strftime("%S"))
 
-        self.fullDate : date = date(year=self.year, month=self.month, day=self.date)
-        self.fullTime : time = time(hour=self.hour, minute=self.minute, second=self.second)
+        self.fullDate : str = date(year=self.year, month=self.month, day=self.date).isoformat()
+        self.fullTime : str = time(hour=self.hour, minute=self.minute, second=self.second).isoformat()
 
     # Query Functions
 
-    def GetNowAsString(self, IncludeSecond : bool) -> str:
-        if IncludeSecond:
+    def GetNowAsString(self, IncludeSeconds : bool = False) -> str:
+        if not isinstance(IncludeSeconds, bool): 
+            return self.now.strftime(DateTime._TimeFormat)
+        if IncludeSeconds:
             return self.now.strftime(DateTime._FullTimeFormat)
         return self.now.strftime(DateTime._TimeFormat)
     
-    def GetTodayAsString(self, IncludeWeekday) -> str:
+    def GetTodayAsString(self, IncludeWeekday : bool = False) -> str:
+        if not isinstance(IncludeWeekday, bool): 
+            return self.now.strftime(DateTime._DateFormat)
         if IncludeWeekday:
             return self.now.strftime(DateTime._FullDateFormat)
         return self.now.strftime(DateTime._DateFormat)
     
     def GetDateTimeAsDict(self) -> dict:
         return {
-            "date": self.fullDate.isoformat(),
-            "time": self.fullTime.strftime(self._TimeFormat)
+            "date": self.fullDate,
+            "time": self.fullTime
         }
+    
+    @staticmethod
+    def FromDictToDateTime(DateTimeDict : dict) -> DateTime:
+        if "date" not in DateTimeDict or "time" not in DateTimeDict:
+            return False
+        DateTimeDict = DateTime(datetime.fromisoformat(f"{DateTimeDict["date"]}T{DateTimeDict["time"]}"))
+        return DateTimeDict
     
     # Date Time Calculation Functions
 
-    def Add(self, _Attribute : str, _Value : int) -> datetime:
+    def Add(self, **kwargs) -> DateTime:
         try:
-            _New_Time = self.now + timedelta(**{_Attribute:_Value})
-            return _New_Time
-        except Exception as _E:
-            print(_E)
+            New_DateTime = self.now + timedelta(**kwargs)
+            return DateTime(New_DateTime)
+        except Exception:
+            return False
 
-    def Subtract(self, _Attribute : str, _Value : int) -> datetime:
+    def Subtract(self, **kwargs) -> DateTime:
         try:
-            _New_Time = self.now - timedelta(**{_Attribute:_Value})
-            return _New_Time
-        except Exception as _E:
-            print(_E)
+            New_DateTime = self.now - timedelta(**kwargs)
+            return DateTime(New_DateTime)
+        except Exception:
+            return False
 
     # Date Time Comparation Functions
-    def DiffWithDateTime(self, _OtherDatetime : datetime, _Unit : str) -> tuple:
-        _DateTimeDiff = self.now - _OtherDatetime
-        _Diff_In_Seconds = _DateTimeDiff.total_seconds()
-        _Difference = 0
-        
-        # Unit Comparation
+    def DiffWithDateTime(self, OtherDatetime : DateTime, Unit : str) -> tuple:
+        DateTimeDifference : timedelta = self.now - OtherDatetime.now
+        Difference : int = 0
+        Status : str = ""
 
-        match _Unit.lower():
-            case "seconds": _Difference = int(_Diff_In_Seconds)
-            case "minutes": _Difference = int(_Diff_In_Seconds // 60)
-            case "hours": _Difference = int(_Diff_In_Seconds // 3600)
-            case "days": _Difference = _DateTimeDiff.days
+        match Unit.lower():
+            case "days":
+                Difference = int(DateTimeDifference.total_seconds() / 864000)
+            case "hours":
+                Difference = int(DateTimeDifference.total_seconds() / 3600)
+            case "minutes":
+                Difference = int(DateTimeDifference.total_seconds() / 60)
+            case "seconds":
+                Difference = int(DateTimeDifference.total_seconds())
             case _:
-                print("Error in function 'DateTime.DiffWithDateTime': Invalid unit received. Return day difference.")
-                _Difference = _DateTimeDiff.days
+                Difference = int(DateTimeDifference.total_seconds() / 864000)
         
         # Compare
-        if _Difference == 0:
-            _Status = "Exact"
-        elif _Difference > 0:
-            _Status = "Ahead"
-        elif _Difference < 0:
-            _Status = "Behind"
+        if Difference == 0:
+            Status = "Exact"
+        elif Difference > 0:
+            Status = "Ahead"
+        elif Difference < 0:
+            Status = "Behind"
 
-        return _Difference, _Status
+        return Status, Difference
 
     def __str__(self):
-        return f"{self.fullTime} {self.fullDate}"
+        return f"{self.fullDate}T{self.fullTime}"
